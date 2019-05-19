3a2538cc00520fc34da266548ac7edec91b78cc6
Steve Tyler <steve.tyler@centrica.com>
2019-05-19 18:22:11 +0100

    added application route and adapter, updated adapter for swapi

---
+++ app/adapters/application.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPIAdapter.extend({
+  init() {
+    this._super(...arguments);
+
+    this.set('headers', {
+      'Accept': 'application/json',
+      'Content-Type' : 'application/json'
+    });
+  },
+  namespace: 'api',
+  host: 'https://swapi.co'
+});
