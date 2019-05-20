6b0a89254824a81036b18b5ff5b246269ac48fe4
Steve Tyler <steve.tyler@centrica.com>
2019-05-20 23:15:46 +0100

    added tests for new routes

--- tests/unit/loading/route-test.js
+++ tests/unit/loading/route-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | person', function(hooks) {
+module('Unit | Route | loading', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:person');
+    let route = this.owner.lookup('route:loading');
     assert.ok(route);
   });
 });
--- tests/unit/people/route-test.js
+++ tests/unit/people/route-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | starship', function(hooks) {
+module('Unit | Route | people', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:starship');
+    let route = this.owner.lookup('route:people');
     assert.ok(route);
   });
 });
