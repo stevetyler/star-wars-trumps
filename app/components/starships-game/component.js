ac5a2c009fd1fbf6d1ff9f385ca9354abc63f2e0
Steve Tyler <steve.tyler@centrica.com>
2019-05-21 23:28:58 +0100

    renamed and updated component to get random collection, updated styles

---
+++ app/components/starships-game/component.js
@@ -0,0 +1,30 @@
+import Component from '@ember/component';
+import {computed} from '@ember/object';
+
+export default Component.extend({
+  leftPlayerScore: 0,
+  rightPlayerScore: 0,
+
+  currentModel: null,
+
+  shuffledModel: computed('starships', function() {
+    const array = this.starships.toArray();
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+  }),
+
+  gameCollection: null,
+
+  actions: {
+    pickRandom() {
+      const shuffledModel = this.shuffledModel;
+
+      this.set('gameCollection', [shuffledModel.get('firstObject'), shuffledModel.get('lastObject')]);
+    },
+  }
+});
