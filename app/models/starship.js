f8dfbe76613a28a83f8b97686c601c0608a408a3
Steve Tyler <steve.tyler@centrica.com>
2019-05-23 09:25:02 +0100

    removed data from models and started to implement integration tests

--- app/models/starship.js
+++ app/models/starship.js
@@ -10,33 +10,3 @@ export default Model.extend({
   passengers: DS.attr('number'),
   hyperdrive_rating: DS.attr('string')
 });
-
-
-/*
-HTTP/1.0 200 OK
-Content-Type: application/json
-{
-    "MGLT": "10 MGLT",
-    "cargo_capacity": "1000000000000",
-    "consumables": "3 years",
-    "cost_in_credits": "1000000000000",
-    "created": "2014-12-10T16:36:50.509000Z",
-    "crew": "342953",
-    "edited": "2014-12-10T16:36:50.509000Z",
-    "hyperdrive_rating": "4.0",
-    "length": "120000",
-    "manufacturer": "Imperial Department of Military Research, Sienar Fleet Systems",
-    "max_atmosphering_speed": "n/a",
-    "model": "DS-1 Orbital Battle Station",
-    "name": "Death Star",
-    "passengers": "843342",
-    "films": [
-        "https://swapi.co/api/films/1/"
-    ],
-    "pilots": [],
-    "starship_class": "Deep Space Mobile Battlestation",
-    "url": "https://swapi.co/api/starships/9/"
-}
-
-
-*/
--- tests/integration/components/game-container/component-test.js
+++ tests/integration/components/game-container/component-test.js
@@ -2,25 +2,89 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | game-container', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<GameContainer />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  // STARSHIPS
+  test('it displays cards correctly', async function(assert) {
+    this.set('game', 'starships');
+    const collection = [
+      EmberObject.create({
+        name: 'Millenium Falcon',
+        crew: '5',
+        length: '',
+        passengers: '',
+        hyperdrive_rating: '',
+        cost_in_credits: '',
+        cargo_capacity: ''
+      }),
+      EmberObject.create({
+        name: '',
+        crew: '1',
+        length: '',
+        passengers: '',
+        hyperdrive_rating: '',
+        cost_in_credits: '',
+        cargo_capacity: ''
+      })
+    ]
+    this.set('gameCollection', collection);
+
     await render(hbs`
-      <GameContainer>
-        template block text
-      </GameContainer>
+      <GameContainer
+        @game="starships"
+        @selectedAttr="crew"/>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    // assert left player attributes
+    // assert right player attributes
+  });
+
+  test('it displays the correct winner', async function(assert) {
+
+  });
+
+
+  // PEOPLE
+  test('it displays cards correctly', async function(assert) {
+    this.set('game', 'people');
+    const collection = [
+      EmberObject.create({
+        name: '',
+        height: '',
+        mass: '',
+        eye_color: '',
+        birth_year: '',
+        gender: ''
+      }),
+      EmberObject.create({
+        name: '',
+        height: '',
+        mass: '',
+        eye_color: '',
+        birth_year: '',
+        gender: ''
+      })
+    ]
+    this.set('gameCollection', collection);
+    await render(hbs`
+      <GameContainer
+        @game="people"
+        @selectedAttr="crew"/>
+    `);
+  });
+
+  test('it displays the correct winner', async function(assert) {
+
   });
 });
