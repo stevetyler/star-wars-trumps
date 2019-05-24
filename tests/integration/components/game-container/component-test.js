94e68282927fcffc22e7f3221c73105c80c4fd54
Steve Tyler <steve.tyler@centrica.com>
2019-05-24 21:57:13 +0100

    updated integration test

--- tests/integration/components/game-container/component-test.js
+++ tests/integration/components/game-container/component-test.js
@@ -7,84 +7,80 @@ import EmberObject from '@ember/object';
 module('Integration | Component | game-container', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    await render(hbs`<GameContainer />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
-
   // STARSHIPS
-  test('it displays cards correctly', async function(assert) {
-    this.set('game', 'starships');
+  test('it displays starship cards correctly', async function(assert) {
     const collection = [
       EmberObject.create({
         name: 'Millenium Falcon',
         crew: '5',
-        length: '',
-        passengers: '',
-        hyperdrive_rating: '',
-        cost_in_credits: '',
-        cargo_capacity: ''
+        length: '300',
+        passengers: '2',
+        hyperdrive_rating: '3',
+        cost_in_credits: '10000',
+        cargo_capacity: '2000'
       }),
       EmberObject.create({
-        name: '',
+        name: 'X Wing',
         crew: '1',
-        length: '',
-        passengers: '',
-        hyperdrive_rating: '',
-        cost_in_credits: '',
-        cargo_capacity: ''
+        length: '100',
+        passengers: '1',
+        hyperdrive_rating: '1',
+        cost_in_credits: '2000',
+        cargo_capacity: '10'
       })
-    ]
-    this.set('gameCollection', collection);
+    ];
+
+    this.set('collection', collection);
 
     await render(hbs`
       <GameContainer
         @game="starships"
-        @selectedAttr="crew"/>
+        @selectedAttr="crew"
+        @gameCollection={{collection}}
+      />
     `);
+
+    assert.equal(this.element.querySelector('h2').textContent.trim(), 'Player :');
 
     // assert left player attributes
     // assert right player attributes
   });
 
-  test('it displays the correct winner', async function(assert) {
-
-  });
+  // test('it displays the correct winner', async function(assert) {
+  //
+  // });
 
 
   // PEOPLE
-  test('it displays cards correctly', async function(assert) {
-    this.set('game', 'people');
-    const collection = [
-      EmberObject.create({
-        name: '',
-        height: '',
-        mass: '',
-        eye_color: '',
-        birth_year: '',
-        gender: ''
-      }),
-      EmberObject.create({
-        name: '',
-        height: '',
-        mass: '',
-        eye_color: '',
-        birth_year: '',
-        gender: ''
-      })
-    ]
-    this.set('gameCollection', collection);
-    await render(hbs`
-      <GameContainer
-        @game="people"
-        @selectedAttr="crew"/>
-    `);
-  });
-
-  test('it displays the correct winner', async function(assert) {
-
-  });
+  // test('it displays cards correctly', async function(assert) {
+  //   this.set('game', 'people');
+  //   const collection = [
+  //     EmberObject.create({
+  //       name: '',
+  //       height: '',
+  //       mass: '',
+  //       eye_color: '',
+  //       birth_year: '',
+  //       gender: ''
+  //     }),
+  //     EmberObject.create({
+  //       name: '',
+  //       height: '',
+  //       mass: '',
+  //       eye_color: '',
+  //       birth_year: '',
+  //       gender: ''
+  //     })
+  //   ]
+  //   this.set('gameCollection', collection);
+  //   await render(hbs`
+  //     <GameContainer
+  //       @game="people"
+  //       @selectedAttr="crew"/>
+  //   `);
+  // });
+  //
+  // test('it displays the correct winner', async function(assert) {
+  //
+  // });
 });
