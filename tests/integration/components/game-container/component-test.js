64e89e1fbfa7b8cff9502897a2d7724c4edae492
Steve Tyler <steve.tyler@centrica.com>
2019-05-24 23:28:15 +0100

    updated integration tests

--- tests/integration/components/game-container/component-test.js
+++ tests/integration/components/game-container/component-test.js
@@ -7,30 +7,88 @@ import EmberObject from '@ember/object';
 module('Integration | Component | game-container', function(hooks) {
   setupRenderingTest(hooks);
 
-  // STARSHIPS
-  test('it displays starship cards correctly', async function(assert) {
-    const collection = [
-      EmberObject.create({
-        name: 'Millenium Falcon',
-        crew: '5',
-        length: '300',
-        passengers: '2',
-        hyperdrive_rating: '3',
-        cost_in_credits: '10000',
-        cargo_capacity: '2000'
-      }),
-      EmberObject.create({
-        name: 'X Wing Fighter',
-        crew: '1',
-        length: '100',
-        passengers: '1',
-        hyperdrive_rating: '1',
-        cost_in_credits: '2000',
-        cargo_capacity: '10'
-      })
-    ];
+  const people = [
+    EmberObject.create({
+      name: 'Obi Wan',
+      height: '200',
+      mass: '80',
+      eye_color: 'blue',
+      birth_year: 'BBY60',
+      gender: 'male'
+    }),
+    EmberObject.create({
+      name: 'Luke Skywalker',
+      height: '140',
+      mass: '60',
+      eye_color: 'blue',
+      birth_year: 'BBY40',
+      gender: 'male'
+    })
+  ];
 
-    this.set('collection', collection);
+  const starships = [
+    EmberObject.create({
+      name: 'Millenium Falcon',
+      crew: '5',
+      length: '300',
+      passengers: '2',
+      hyperdrive_rating: '3',
+      cost_in_credits: '10000',
+      cargo_capacity: '2000'
+    }),
+    EmberObject.create({
+      name: 'X Wing Fighter',
+      crew: '1',
+      length: '100',
+      passengers: '1',
+      hyperdrive_rating: '1',
+      cost_in_credits: '2000',
+      cargo_capacity: '10'
+    })
+  ];
+
+  // PEOPLE
+  test('it displays people correctly', async function(assert) {
+    this.set('collection', people);
+
+    await render(hbs`
+      <GameContainer
+        @game="people"
+        @selectedAttr="mass"
+        @gameCollection={{collection}}
+      />
+    `);
+
+    assert.expect(12);
+    assert.equal(this.element.querySelector('.row .col:nth-child(1) h3').textContent.trim(), 'Obi Wan');
+    assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(1)').textContent.trim(), 'Mass : 80');
+    assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(2)').textContent.trim(), 'Gender : male');
+    assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(3)').textContent.trim(), 'Height : 200');
+    assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(4)').textContent.trim(), 'Eye Colour : blue');
+    assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(5)').textContent.trim(), 'Birth Year : BBY60');
+
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) h3').textContent.trim(), 'Luke Skywalker');
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(1)').textContent.trim(), 'Mass : 60');
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(2)').textContent.trim(), 'Gender : male');
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(3)').textContent.trim(), 'Height : 140');
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(4)').textContent.trim(), 'Eye Colour : blue');
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(5)').textContent.trim(), 'Birth Year : BBY40');
+  });
+
+  // test('it displays the correct winner', async function(assert) {
+  //   this.set('collection', people);
+  //
+  //   await render(hbs`
+  //     {{game-container gameCollection=collection game="people"}}
+  //   `);
+  //   assert.async();
+  //   assert.equal(this.element.querySelector('p').textContent.trim(), 'Player Wins');
+  // });
+
+
+  // STARSHIPS
+  test('it displays starships correctly', async function(assert) {
+    this.set('collection', starships);
 
     await render(hbs`
       <GameContainer
@@ -49,7 +107,7 @@ module('Integration | Component | game-container', function(hooks) {
     assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(5)').textContent.trim(), 'Cost in Credits : 10000');
     assert.equal(this.element.querySelector('.row .col:nth-child(1) li:nth-child(6)').textContent.trim(), 'Cargo Capacity : 2000');
 
-    assert.equal(this.element.querySelector('.row .col:nth-child(1) h3').textContent.trim(), 'X Wing Fighter');
+    assert.equal(this.element.querySelector('.row .col:nth-child(2) h3').textContent.trim(), 'X Wing Fighter');
     assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(1)').textContent.trim(), 'Crew : 1');
     assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(2)').textContent.trim(), 'Length : 100');
     assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(3)').textContent.trim(), 'Passengers : 1');
@@ -58,40 +116,6 @@ module('Integration | Component | game-container', function(hooks) {
     assert.equal(this.element.querySelector('.row .col:nth-child(2) li:nth-child(6)').textContent.trim(), 'Cargo Capacity : 10');
   });
 
-  // test('it displays the correct winner', async function(assert) {
-  //
-  // });
-
-
-  // PEOPLE
-  // test('it displays cards correctly', async function(assert) {
-  //   this.set('game', 'people');
-  //   const collection = [
-  //     EmberObject.create({
-  //       name: '',
-  //       height: '',
-  //       mass: '',
-  //       eye_color: '',
-  //       birth_year: '',
-  //       gender: ''
-  //     }),
-  //     EmberObject.create({
-  //       name: '',
-  //       height: '',
-  //       mass: '',
-  //       eye_color: '',
-  //       birth_year: '',
-  //       gender: ''
-  //     })
-  //   ]
-  //   this.set('gameCollection', collection);
-  //   await render(hbs`
-  //     <GameContainer
-  //       @game="people"
-  //       @selectedAttr="crew"/>
-  //   `);
-  // });
-  //
   // test('it displays the correct winner', async function(assert) {
   //
   // });
