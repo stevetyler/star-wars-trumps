501b750de170a2b96ca760d254a003b236bfc9ac
Steve Tyler <steve.tyler@centrica.com>
2019-05-25 16:26:39 +0100

    added random integer util and updated routes to query different page numbers

--- app/people/route.js
+++ app/people/route.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import getRandomInt from 'star-wars-trumps/utils/random-integer';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('person');
+    // 87 people
+    const pageNum = getRandomInt(1, 8);
+
+    return this.store.query('person', {page: pageNum});
   }
 });
--- app/starships/route.js
+++ app/starships/route.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import getRandomInt from 'star-wars-trumps/utils/random-integer';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('starship');
+    // 37 starships
+    const pageNum = getRandomInt(1, 3);
+
+    return this.store.query('starship', {page: pageNum});
   }
 });
