07bad42a76ebf9d4443e09f0b1bc57bda707cd51
Steve Tyler <steve.tyler@centrica.com>
2019-05-25 17:03:39 +0100

    added unit test for random-integer util

---
+++ tests/unit/utils/random-integer-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import getRandomInt from 'star-wars-trumps/utils/random-integer';
+
+module('Unit | Utils | Random Integer', function(hooks) {
+  setupTest(hooks);
+
+  test('it returns a random number greater than or equal to the minimum argument', function(assert) {
+    for (let i = 0; i < 20; i++) {
+      assert.ok(getRandomInt(1,10) >= 1, 'value is greater than or equal to minimum');
+    }
+  });
+
+  test('it returns a random number less than or equal to the maximum argument', function(assert) {
+    for (let i = 0; i < 20; i++) {
+      assert.ok(getRandomInt(1,10) <= 10, 'value is less than or equal to maximum');
+    }
+  });
+});
