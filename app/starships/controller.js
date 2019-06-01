eabe761b0260c78df4569ed52f1bdefe041ea0f7
Steve Tyler <steve.tyler@centrica.com>
2019-06-01 10:46:33 +0100

    updated sratships route

--- app/starships/controller.js
+++ app/starships/controller.js
@@ -1,8 +1,14 @@
 import Controller from '@ember/controller';
-import {computed} from '@ember/object';
+import {sort} from '@ember/object/computed';
 
 export default Controller.extend({
-  shuffledModel: computed('model', function() {
-    
-  })
+  shuffledModel: sort('model', function () {
+    const array = this.model.toArray();
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }),
 });
