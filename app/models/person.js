6390a10a38c828cbd0db80e7d839c0ba8eb7f857
Steve Tyler <steve.tyler@centrica.com>
2019-06-05 22:14:04 +0100

    changed model attrs to strings

--- app/models/person.js
+++ app/models/person.js
@@ -3,8 +3,8 @@ const { Model } = DS;
 
 export default Model.extend({
   name: DS.attr('string'),
-  height: DS.attr('number'),
-  mass: DS.attr('number'),
+  height: DS.attr('string'),
+  mass: DS.attr('string'),
   eye_color: DS.attr('string'),
   birth_year: DS.attr('string'),
   gender: DS.attr('string'),
