4f0853072abdbb73925f8023714329ab99f07e2b
Steve Tyler <steve.tyler@centrica.com>
2019-07-04 23:19:04 +0100

    updated models with properties object which can be iterated

--- app/models/person.js
+++ app/models/person.js
@@ -2,10 +2,20 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
+  init() {
+    this._super(...arguments);
+    this.set('properties', {
+      Gender: this.get('gender'),
+      'Eye Colour': this.get('eye_color'),
+      Height: this.get('height'),
+      Mass: this.get('mass'),
+      'Birth Year': this.get('birth_year'),
+    })
+  },
   name: DS.attr('string'),
   height: DS.attr('string'),
   mass: DS.attr('string'),
   eye_color: DS.attr('string'),
   birth_year: DS.attr('string'),
-  gender: DS.attr('string'),
+  gender: DS.attr('string')
 });
--- app/models/starship.js
+++ app/models/starship.js
@@ -2,6 +2,16 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
+  init() {
+    this._super(...arguments);
+    this.set('properties', {
+      Passengers: this.get('passengers'),
+      Length: this.get('length'),
+      'Hyperdrive Rating': this.get('hyperdrive_rating'),
+      'Cost in Credits': this.get('cost_in_credits'),
+      'Cargo Capacity': this.get('cargo_capacity'),
+    })
+  },
   name: DS.attr('string'),
   cargo_capacity: DS.attr('string'),
   cost_in_credits: DS.attr('string'),
